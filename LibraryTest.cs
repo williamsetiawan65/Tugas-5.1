48eee760a585cf9ac249621267946cec26377c60
williamsetiawan65 <92424828+williamsetiawan65@users.noreply.github.com>
2021-10-27 23:45:32 +0700

    Add files via upload

---
+++ LibraryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class LibraryTest
+    {
+        private Day day;
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+        }
+        [Test]
+        public void DayTest()
+        {
+            Assert.AreEqual("Selasa", day.NameOfDay(2));
+        }
+        [Test]
+        public void UserTest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+        }
+    }
+}
